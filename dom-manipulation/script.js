73b4975b0fe129ce33f148af657fa9cf3603d785
Ceca Cora <hello@cecacora.com>
2021-01-06 03:43:07 +0800

    Completing DOM Manipulation Exercise!

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -4,4 +4,27 @@ const content = document.createElement('div');
 content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
 
+const redText = document.createElement('p');
+redText.style.color = 'red';
+redText.textContent = "Hey I'm red!";
+
+const blueText = document.createElement('h3');
+blueText.style.color = 'blue';
+blueText.textContent = "I'm a blue h3!";
+
+const specialDiv = document.createElement('div');
+specialDiv.style.cssText = 'border: 1px solid black; background: pink;';
+
+const h1InDiv = document.createElement('h1');
+h1InDiv.textContent = "I'm in a div";
+
+const pInDiv = document.createElement('p');
+pInDiv.textContent = 'ME TOO!';
+
+specialDiv.appendChild(h1InDiv);
+specialDiv.appendChild(pInDiv);
+
 container.appendChild(content);
+container.appendChild(redText);
+container.appendChild(blueText);
+container.appendChild(specialDiv);
