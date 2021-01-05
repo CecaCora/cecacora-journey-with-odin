968f7c8970c1d4dd6129b689bb9cfed7cfdf317f
Ceca Cora <hello@cecacora.com>
2021-01-06 04:04:00 +0800

    Completing DOM Manipulation Events!

---
+++ dom-manipulation/events.js
@@ -0,0 +1,17 @@
+function alertFunction() {
+  alert('YAY! YOU DID IT!');
+}
+
+const btn = document.querySelector('#btn');
+
+btn.addEventListener('click', function (e) {
+  e.target.style.background = 'blue';
+});
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    alert(button.id);
+  });
+});
